416ddaefce223d8c19e425b7c8b6f5d9f4e6f6d4
artikus13 <artikus.sol@gmail.com>
2020-04-23 14:10:55 +0400

    Эпизод 1. Разворачивание проекта

---
+++ art-frontend-create-posts.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Art Frontend Create Posts
+ * Plugin URI: wpruse.ru
+ * Text Domain:
+ * Domain Path: /languages
+ * Description: Плагин добавления записей с фронта
+ * Version: 1.0.0
+ * Author: Artem Abramovich
+ * Author URI: https://wpruse.ru/
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
