85c653a3c730325d1f749b264bf0af69c3be0825
artikus13 <artikus.sol@gmail.com>
2020-06-04 16:42:15 +0400

    Эпизод 4. Валидация картинок

--- includes/class-afcp-ajax.php
+++ includes/class-afcp-ajax.php
@@ -11,11 +11,14 @@ class AFCP_Ajax {
 
 	public function callback() {
 
-		error_log( print_r( $_POST, 1 ) );
+		error_log( print_r( $_FILES, 1 ) );
 
 		check_ajax_referer( 'afcp-ajax-nonce', 'nonce' );
 
-		$this->validation();
+		//$this->validation();
+		if ( ! empty( $_FILES ) ) {
+			$this->validation_thumbnail();
+		}
 
 		wp_die();
 	}
@@ -26,13 +29,13 @@ class AFCP_Ajax {
 		$error = [];
 
 		$required = [
-			'event_title'        => 'Это обязательное поле. Укажите заголовок мероприятия',
-			'event_topics'       => 'Это обязательное поле. Выберите нужную категорию',
+			'event_title'    => 'Это обязательное поле. Укажите заголовок мероприятия',
+			'event_topics'   => 'Это обязательное поле. Выберите нужную категорию',
 			//'event_hashtags'     => 'Это обязательное поле. Укажите метку в виде хештега, в формате #вашаМетка',
 			//'event_descriptions' => 'Это обязательное поле. Напишите о чем, это мероприятие',
 			//'event_thumbnail'    => 'Это обязательное поле. Укажите миниатюру мероприятия',
-			'event_date'         => 'Это обязательное поле. Укажите дату мероприятия',
-			'event_location'     => 'Это обязательное поле. Укажите меато проведения мероприятия',
+			'event_date'     => 'Это обязательное поле. Укажите дату мероприятия',
+			'event_location' => 'Это обязательное поле. Укажите меато проведения мероприятия',
 		];
 
 		foreach ( $required as $key => $item ) {
@@ -45,6 +48,26 @@ class AFCP_Ajax {
 		if ( $error ) {
 			$this->error( $error );
 		}
+	}
+
+
+	public function validation_thumbnail() {
+
+		$size     = getimagesize( $_FILES['event_thumbnail']['tmp_name'] );
+		$max_size = 800;
+		$type     = $_FILES['event_thumbnail']['type'];
+
+		if ( $size[0] > $max_size || $size[1] > $max_size ) {
+			$image_message = 'Изображение не может быть больше ' . $max_size . 'рх в высоту или ширину';
+			$this->remove_image( $image_message );
+		}
+
+		if ( 'image/jpeg' !== $type || 'image/png' !== $type ) {
+
+			$image_message = 'Неверный формат файла. Жопускаются только файлы изображений в формате .jpg, .png';
+			$this->remove_image( $image_message );
+		}
+
 	}
 
 
@@ -69,5 +92,13 @@ class AFCP_Ajax {
 			]
 		);
 
+	}
+
+
+	public function remove_image( $image_message ) {
+
+		unlink( $_FILES['event_thumbnail']['tmp_name'] );
+
+		$this->error( $image_message );;
 	}
 }
